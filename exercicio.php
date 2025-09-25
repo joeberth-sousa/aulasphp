49e7f66188d4968ec49e7e3e201cce45c6d75004
joeberth-sousa <joeberth607@gmail.com>
2025-09-25 14:17:48 -0300

    25/09

---
+++ exercicio.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercicío de PHP</h1>
+
+    <form action="exercico1a.php">
+
+    <p>
+        Digite o nome do cliente <br>
+        <input type="text " name="Cliente">
+    </p>
+
+    <p>
+        escolha o sexo do cliente <br>
+    <input type="radio" name="sexo" value="Feminino">Feminino<br>
+    <input type="radio" name="sexo" value="Masculino">Masculino<br>
+    
+
+    <p> 
+    Digite o valor da compra <br>
+        <input type="number " name="valorcompra">
+    </p>
+
+    <p>
+        <input type="submit" value="Enviar">
+
+    </p>
+</form>
+
+        
+
+
+    
+</body>
+</html>
---
+++ exercicio1a.php
@@ -0,0 +1,9 @@
+<?php
+
+$cliente= $_REQUEST["Cliente"];
+$sexo= $_REQUEST["sexo"];
+$Valor = $_REQUEST["valorcompra"];
+
+
+
+?>
