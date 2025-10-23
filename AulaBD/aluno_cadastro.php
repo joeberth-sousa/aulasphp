e5c6496f6a3245d99a20add248c19fc40624ae26
joeberth-sousa <joeberth607@gmail.com>
2025-10-23 14:20:37 -0300

    23/10

---
+++ AulaBD/aluno_cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "menu.php" ?>
+
+    <div class="conteudo">
+
+    <h3> cadastro de alunos<h3>
+        <form action="#" method="post">
+            <p>
+                Digite o nome do aluno <br>
+                <input type="text" name="nome">
+</p>    
+
+<p>
+                Digite a cidade <br>
+                <input type="text" name="cidade">
+</p> 
+
+<p>
+                Digite o estado <br>
+                <input type="text" name="estado">
+</p>    
+</body>
+</html>
